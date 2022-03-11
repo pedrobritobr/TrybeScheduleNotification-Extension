c76bacb882e631cb5c32365a1fc14f5ba17bdc4b
pedrobrito <pedroab0119@gmail.com>
2022-03-10 23:47:54 -0300

    :recycle: refact - formatScheduleString Func

--- background.js
+++ background.js
@@ -3,10 +3,9 @@
 function formatScheduleString(scheduleDayDiv) {
   const MANY_WHITE_SPACES = /\s\s\s\s+/;
   const NUMBER_OR_BRACKET = /^\d\d|^[[]/;
-  const ZOOM_PATTERN = /^\s[|]\s/;
+  const ZOOM_PATTERN = /(^ [|] Zoom)|( [|] Zoom$)/gim;
   // MONTAR PADRÃO ZOOM
   const agendaStrings = scheduleDayDiv.innerText.split('\n');
-
   // BY REMOVING STRINGS WITH SPACES
   const scheduleTrybeNoSpaces = agendaStrings
     .filter((trybeString) => !trybeString.match(MANY_WHITE_SPACES));
@@ -36,35 +35,7 @@ function getZoomLinks(scheduleDayDiv) {
 
 function getLastScheduleDay() {
   const BLOCK_KIT_RENDER = document.getElementsByClassName('p-block_kit_renderer__block_wrapper');
-
   const QUEM_REAGUE_COM = /(quem\s[a-z]+)( |, )(reage com)/img;
-
-  // const agenda = 'AGENDA DO DIA |';
-
-  // const P_RICH_TEXT_SECTION = document.getElementsByClassName('p-rich_text_block');
-  // const agenda = 'reage com';
-
-  // CLASSES
-  //   p-rich_text_block
-  //   p-rich_text_section
-  //   p-block_kit_renderer__block_wrapper ----- TALVEZ ESSE
-  //   p-block_kit_renderer__block_wrapper--first
-  //   p-block_kit_renderer
-  //   c-message__message_blocks
-  //   c-message__message_blocks--rich_text
-  //   c-message_kit__blocks
-  //   c-message_kit__blocks--rich_text
-  //   c-message_kit__gutter__right
-  //   c-message_kit__gutter
-  //   c-message_kit__actions
-  //   c-message_kit__actions--default
-  //   c-message_kit__hover
-
-  //   c-mrkdwn__quote
-  // TAG
-  //   blockquote
-
-  // role=document
 
   const lastScheduleDay = Array.from(BLOCK_KIT_RENDER)
     .filter((e) => e.innerText.match(QUEM_REAGUE_COM)).at(-1);
@@ -84,5 +55,3 @@ function main() {
 }
 
 main();
-
-// \\wsl$\Ubuntu-20.04\home\brito\desktop\myproj
