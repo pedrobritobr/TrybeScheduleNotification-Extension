a0bfe808c1815df78c42ac48bdb6aa252fb0a337
pedrobrito <pedroab0119@gmail.com>
2022-03-22 13:36:40 -0300

    :mute: remove console.log from background

--- background.js
+++ background.js
@@ -3,17 +3,11 @@
 
 try {
   chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
-    // 2. A page requested user data, respond with a copy of `user`
-    console.log('Ouvindo msg: ', message);
     if (message === 'toTheBackground') {
-      // console.log('Msg recebida, enviando "user"');
-      console.log('chrome.alarms BACKGROUND: ', chrome.alarms);
       chrome.alarms.getAll((resp) => console.log('resp Alarms: ', resp));
       sendResponse('Saindo do background');
     }
   });
-
-  // console.log(chrome);
 } catch (error) {
   console.log(error);
 }
