6817c3593dd8d0a5d6bde5b2085a32814cc35c37
pedrobrito <pedroab0119@gmail.com>
2022-05-12 11:58:46 -0300

    :sparkles: click in "Iniciar a reuniao" when page loads

---
+++ src/content.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+  if (window.location.href.includes('trybe.zoom.us')) {
+    const launchZoom = document.querySelector('div.mbTuDeF1');
+    launchZoom.click();
+  }
+});
